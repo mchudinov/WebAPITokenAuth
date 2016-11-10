6de3d296bd9845f087c9a506ad2dea6a2d4fab62
Mikael Chudinov <mikael@chudinov.net>
2016-11-11 00:50:19 +0100

    Pass token to API.

--- Api/Controllers/DefaultController.cs
+++ Api/Controllers/DefaultController.cs
@@ -1,13 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Web.Http;
 
 namespace Api.Controllers
 {
     public class DefaultController : ApiController
     {
+        [Authorize]
         public IEnumerable<string> Get()
         {
             return new [] { "value1", "value2" };
+
+            //IEnumerable<string> headerValues = request.Headers.GetValues("X-My-Custom-Header");
+            //if (null != headerValues)
+            //{
+            //    var encoding = Encoding.GetEncoding("iso-8859-1");
+            //    string samlToken = encoding.GetString(Convert.FromBase64String(headerValues.FirstOrDefault()));
+            //}
+
         }
     }
 }
--- Gui/Controllers/HomeController.cs
+++ Gui/Controllers/HomeController.cs
@@ -13,6 +13,8 @@ namespace Gui.Controllers
             ViewBag.ClaimsIdentity = System.Web.HttpContext.Current.User.Identity;
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:63046/api/Default");
+            request.Headers["X-My-Custom-Header"] = Session["token"].ToString();
+            request.Headers.Add(HttpRequestHeader.Authorization, Session["token"].ToString());
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             Stream stream = response.GetResponseStream();
             StreamReader reader = new StreamReader(stream);
