84f0b8543916243b1b072ab095d74be58c2f7701
Mikael Chudinov <mikael@chudinov.net>
2016-11-11 01:02:42 +0100

    Read and validate token.

--- Api/Controllers/DefaultController.cs
+++ Api/Controllers/DefaultController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Authentication;
+using System.Security.Claims;
 using System.Text;
 using System.Web.Http;
+using System.Xml;
 
 namespace Api.Controllers
 {
@@ -21,5 +25,39 @@ namespace Api.Controllers
             //}
 
         }
+
+        //public ClaimsIdentity GetIdentityFromToken(string tokenBase64)
+        //{
+        //    if (string.IsNullOrEmpty(tokenBase64))
+        //        return null;
+
+        //    byte[] tokenByteArray = Convert.FromBase64String(tokenBase64);
+        //    string decodedToken = Encoding.UTF8.GetString(tokenByteArray);
+
+        //    if (string.IsNullOrWhiteSpace(decodedToken))
+        //        return null;
+        //    try
+        //    {
+        //        var handlers = FederatedAuthentication.FederationConfiguration.IdentityConfiguration.SecurityTokenHandlers;
+        //        SecurityToken token;
+        //        using (StringReader stringReader = new StringReader(decodedToken))
+        //        {
+        //            using (XmlTextReader xmlReader = new XmlTextReader(stringReader))
+        //            {
+        //                token = handlers.ReadToken(xmlReader);
+        //            }
+        //        }
+
+        //        if (token == null)
+        //            return null;
+
+        //        return handlers.ValidateToken(token).FirstOrDefault();
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        //logger.Error(new AuthenticationException("Error validating the token from ADFS", e));
+        //        return null;
+        //    }
+        //}
     }
 }
